861a4f910c0db63904afdc53725d03a82fc6b763
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2026-05-29 23:07:42 +0300

    feat: implement drag functionality for the chat modal window
    	- update scripts ('assemblingOrders.js', 'sellersPages.js'):
    		- introduce 'dragChat' function to enable moving the chat element across the screen.
    		- bind the 'onmousedown' event listener specifically to the modal's header ('.modal-header') to prevent interference with text selection in the chat body.
    		- implement 'dragMouseDown', 'ChatDrag', and 'closeDragElement' functions to handle pointer coordinate calculations and dynamically update the modal's CSS top/left absolute positioning.

--- shop/src/main/resources/static/js/sellersPages.js
+++ shop/src/main/resources/static/js/sellersPages.js
@@ -303,4 +303,46 @@ function showMessage(sender, content) {
     // прокрутка вниз
     chatArea.scrollTop = chatArea.scrollHeight;
 }
+
+
+// Перетаскивание Чата
+dragChat(document.getElementById("chatModal"));
+function dragChat(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var header = elmnt.querySelector('.modal-header');
+
+    if (header) {
+        header.onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // Оставляем возможность нажимать на крестик
+        if (e.target.className === 'close-btn') return;
+
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = ChatDrag;
+    }
+
+    function ChatDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
 // CHAT             ↑↑↑↑↑
